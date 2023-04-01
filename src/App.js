85da74f72e2567ccd53e34d24e1223ba63e2a1df
raed-github <raed__saad@hotmail.com>
2023-04-02 03:01:45 +0400

    part-2 create navbar and a landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <div className='pages'>
+          {/* we need to put the nav bar component inside the router since it contains links which must be placed inside the routers tag */}
+          <Navbar/>
           <Routes>
             <Route
               path="/"
