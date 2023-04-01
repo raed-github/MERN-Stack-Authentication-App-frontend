58c138c26c9ef16fd1c10abbe12b2393a49a31ca
raed-github <raed__saad@hotmail.com>
2023-04-02 03:44:17 +0400

    part-3 load tasks from database by applying data fetching

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react'
+import TaskItem from '../components/TaskItem'
+
 const Home = ()=>{
+    const [tasks,setTasks] = useState()
+
+    useEffect(()=>{
+        const fetchTasks = async ()=>{
+            //proxy is added inside package.json only for dev environment. in production, we must apply cors
+            const response = await fetch('/api/tasks')
+            const json = await response.json()
+
+            if(response.ok){
+                setTasks(json)
+            }
+        }
+        fetchTasks()
+    },[])
     return(
         <div className="home">
-            <h2>Landing page</h2>
+            <div className="tasks">
+                {tasks && tasks.map((task)=>(
+                    <TaskItem key="{task._id}" task={task} />
+                ))}
+            </div>
         </div>
     )
 }
